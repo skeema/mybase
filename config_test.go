skeema/mybase

commit f777a7f377af08ac19d345c05d27ff9e1608b2f7

implement new method Config.SetRuntimeOverride
This commit provides a way to cleanly override option values in a Config
at run-time. These overrides take priority over all other option value sources
in the Config, including the command-line. If the Config is cloned via its
Clone method, a deep copy of its overrides is made; any subsequent overrides
only affect the method receiver, not all clones.

config.go
@@ -17,6 +17,22 @@ type OptionValuer interface {
 	OptionValue(optionName string) (value string, ok bool)
 }
 
+// StringMapValues is the most trivial possible implementation of the
+// OptionValuer interface: it just maps option name strings to option value
+// strings.
+type StringMapValues map[string]string
+
+// OptionValue satisfies the OptionValuer interface, allowing StringMapValues
+// to be an option source for Config methods.
+func (source StringMapValues) OptionValue(optionName string) (string, bool) {
+	val, ok := source[optionName]
+	return val, ok
+}
+
+func (source StringMapValues) String() string {
+	return "runtime override"
+}
+
 // Config represents a list of sources for option values -- the command-line
 // plus zero or more option files, or any other source implementing the
 // OptionValuer interface.
@@ -24,7 +40,8 @@ type Config struct {
 	CLI              *CommandLine            // Parsed command-line
 	IsTest           bool                    // true if Config generated from test logic, false otherwise
 	LooseFileOptions bool                    // enable to ignore unknown options in all Files
-	sources          []OptionValuer          // Sources of option values, excluding CLI or Command; higher indexes override lower indexes
+	runtimeOverrides StringMapValues         // Highest-priority option value overrides
+	sources          []OptionValuer          // Sources of option values, excluding CLI, Command, runtimeOverrides; higher indexes override lower indexes
 	unifiedValues    map[string]string       // Precomputed cache of option name => value
 	unifiedSources   map[string]OptionValuer // Precomputed cache of option name => which source supplied it
 	dirty            bool                    // true if source list has changed, meaning next access needs to recompute caches
@@ -37,9 +54,10 @@ type Config struct {
 // other sources, and should not be supplied redundantly via sources.
 func NewConfig(cli *CommandLine, sources ...OptionValuer) *Config {
 	return &Config{
-		CLI:     cli,
-		sources: sources,
-		dirty:   true,
+		CLI:              cli,
+		runtimeOverrides: StringMapValues(make(map[string]string)),
+		sources:          sources,
+		dirty:            true,
 	}
 }
 
@@ -50,10 +68,15 @@ func NewConfig(cli *CommandLine, sources ...OptionValuer) *Config {
 func (cfg *Config) Clone() *Config {
 	sourcesCopy := make([]OptionValuer, len(cfg.sources))
 	copy(sourcesCopy, cfg.sources)
+	runtimeOverridesCopy := StringMapValues(make(map[string]string, len(cfg.runtimeOverrides)))
+	for rtoName, rtoValue := range cfg.runtimeOverrides {
+		runtimeOverridesCopy[rtoName] = rtoValue
+	}
 	return &Config{
 		CLI:              cfg.CLI,
 		IsTest:           cfg.IsTest,
 		LooseFileOptions: cfg.LooseFileOptions,
+		runtimeOverrides: runtimeOverridesCopy,
 		sources:          sourcesCopy,
 		dirty:            true,
 	}
@@ -100,8 +123,9 @@ func (cfg *Config) rebuild() {
 	// Next come cfg.sources, which are already ordered from lowest priority to highest priority
 	allSources = append(allSources, cfg.sources...)
 
-	// Finally, at highest priority is options provided on the command-line
-	allSources = append(allSources, cfg.CLI)
+	// Finally, at highest priorities are options provided on the command-line,
+	// and then runtime overrides
+	allSources = append(allSources, cfg.CLI, cfg.runtimeOverrides)
 
 	options := cfg.CLI.Command.Options()
 	cfg.unifiedValues = make(map[string]string, len(options)+len(cfg.CLI.Command.args))
@@ -153,8 +177,31 @@ func (cfg *Config) rebuildIfDirty() {
 // MarkDirty causes the config to rebuild itself on next option lookup. This
 // is only needed in situations where a source is known to have changed since
 // the previous lookup.
+//
+// Deprecated: Callers should prefer using SetRuntimeOverride, instead of
+// directly manipulating a source and then calling MarkDirty.
 func (cfg *Config) MarkDirty() {
 	cfg.dirty = true
+}
+
+// SetRuntimeOverride sets an override value for the supplied option name.
+// This value takes precedence over all sources, including option values that
+// were supplied on the CLI. The supplied name must correspond to a known option
+// in cfg, otherwise this method panics.
+func (cfg *Config) SetRuntimeOverride(name, value string) {
+	if _, ok := cfg.unifiedSources[name]; !ok {
+		panic(fmt.Errorf("Assertion failed: option %s does not exist", name))
+	}
+
+	cfg.runtimeOverrides[name] = value
+
+	// Instead of marking the config as dirty and rebuilding it lazily, we can
+	// just set the value right away, since runtime overrides are always the
+	// highest priority source.
+	if !cfg.dirty {
+		cfg.unifiedValues[name] = value
+		cfg.unifiedSources[name] = cfg.runtimeOverrides
+	}
 }
 
 // Changed returns true if the specified option name has been set, and its
@@ -190,9 +237,9 @@ func (cfg *Config) Supplied(name string) bool {
 
 // SuppliedWithValue returns true if the specified option name has been set by
 // some configuration source AND had a value specified, even if that value was
-// a blank string. For example, this returns true even for "--foo=''" or
-// "--foo=" on a command line, or "foo=''" or "foo=" in an option file. Returns
-// false for bare "--foo" on CLI or bare "foo" in an option file.
+// a blank string or empty value. For example, this returns true even for
+// --foo="" or --foo= on a command line, or foo="" or foo= in an option file.
+// Returns false for bare --foo on CLI or bare foo in an option file.
 // This method is only usable on OptionTypeString options with !RequireValue.
 // Panics if the supplied option name does not meet those requirements.
 func (cfg *Config) SuppliedWithValue(name string) bool {

config_test.go
@@ -86,6 +86,58 @@ func TestSuppliedWithValue(t *testing.T) {
 	assertSuppliedWithValue(cfg, "optional2", true)
 }
 
+func TestRuntimeOverride(t *testing.T) {
+	assertOptionValue := func(c *Config, name, expected string) {
+		t.Helper()
+		if actual := c.Get(name); actual != expected {
+			t.Errorf("Expected config.Get(%q) = %q, instead found %q", name, expected, actual)
+		}
+	}
+	assertOnCLI := func(c *Config, name string, expected bool) {
+		t.Helper()
+		if actual := c.OnCLI(name); actual != expected {
+			t.Errorf("Expected config.OnCLI(%q) = %t, instead found %t", name, expected, actual)
+		}
+	}
+
+	cmd := simpleCommand()
+	cmd.AddOption(StringOption("optional1", 'y', "", "dummy description").ValueOptional())
+	cmd.AddOption(StringOption("optional2", 'z', "default", "dummy description").ValueOptional())
+	cfg := ParseFakeCLI(t, cmd, "mycommand -s 'hello world' --skip-truthybool arg1")
+
+	// Confirm results prior to overrides
+	assertOptionValue(cfg, "optional1", "")
+	assertOptionValue(cfg, "optional2", "default")
+	assertOptionValue(cfg, "hasshort", "hello world")
+	assertOnCLI(cfg, "hasshort", true)
+	assertOnCLI(cfg, "optional2", false)
+
+	// Confirm behavior of overrides
+	cfg.SetRuntimeOverride("hasshort", "overridden1")
+	cfg.SetRuntimeOverride("optional2", "overridden2")
+	assertOptionValue(cfg, "hasshort", "overridden1")
+	assertOptionValue(cfg, "optional2", "overridden2")
+	assertOnCLI(cfg, "hasshort", false)
+	assertOnCLI(cfg, "optional2", false)
+
+	// Confirm behaviors of clone, including use of a deep copy of the overrides
+	// map, rather than a shared reference
+	clone := cfg.Clone()
+	assertOptionValue(clone, "optional1", "")
+	assertOptionValue(clone, "hasshort", "overridden1")
+	assertOptionValue(clone, "optional2", "overridden2")
+	assertOnCLI(clone, "hasshort", false)
+	assertOnCLI(clone, "optional2", false)
+	assertOnCLI(clone, "truthybool", true)
+
+	cfg.SetRuntimeOverride("optional2", "newval")
+	assertOptionValue(cfg, "optional2", "newval")
+	assertOptionValue(clone, "optional2", "overridden2")
+	clone.SetRuntimeOverride("hasshort", "alsonew")
+	assertOptionValue(cfg, "hasshort", "overridden1")
+	assertOptionValue(clone, "hasshort", "alsonew")
+}
+
 func TestGetRaw(t *testing.T) {
 	optionValues := map[string]string{
 		"basic":     "foo",

file.go
@@ -313,22 +313,18 @@ func (f *File) OptionValue(optionName string) (string, bool) {
 }
 
 // SetOptionValue sets an option value in the named section. This is not
-// persisted to the file until Write is called on the File.
-// If the caller plans to subsequently read configuration values from this
-// same File object, it is the caller's responsibility to normalize the
-// optionName and value prior to calling this method, and call MarkDirty() on
-// any relevant Configs. These shortcomings will be fixed in a future release.
+// persisted to the file until Write is called on the File. This is not
+// guaranteed to affect any Config that is already using the File as a
+// Source.
 func (f *File) SetOptionValue(sectionName, optionName, value string) {
 	section := f.getOrCreateSection(sectionName)
 	section.Values[optionName] = value
 }
 
 // UnsetOptionValue removes an option value in the named section. This is not
-// persisted to the file until Write is called on the File.
-// If the caller plans to subsequently read configuration values from this
-// same File object, it is the caller's responsibility to normalize the
-// optionName and value prior to calling this method, and call MarkDirty() on
-// any relevant Configs. These shortcomings will be fixed in a future release.
+// persisted to the file until Write is called on the File. This is not
+// guaranteed to affect any Config that is already using the File as a
+// Source.
 func (f *File) UnsetOptionValue(sectionName, optionName string) {
 	section := f.getOrCreateSection(sectionName)
 	delete(section.Values, optionName)

testing.go
@@ -35,14 +35,7 @@ func AssertFileMissingOptions(t *testing.T, file *File, options ...string) {
 
 // SimpleSource is the most trivial possible implementation of the OptionValuer
 // interface: it just maps option name strings to option value strings.
-type SimpleSource map[string]string
-
-// OptionValue satisfies the OptionValuer interface, allowing SimpleSource to
-// be an option source for Config methods.
-func (source SimpleSource) OptionValue(optionName string) (string, bool) {
-	val, ok := source[optionName]
-	return val, ok
-}
+type SimpleSource = StringMapValues
 
 // SimpleConfig returns a stub config based on a single map of key->value string
 // pairs. All keys in the map will automatically be considered valid options.

commit 9300bbbd5258c1f32885e24e03197886cf2a61e3

Config.SetRuntimeOverride: fix panic on dirty config
Recent commit f777a7f introduced new method Config.SetRuntimeOverride(). The
original logic for this method had a bug which would cause a panic if the
method was used on a "dirty" config, or on a config which hadn't had any
option lookups performed on it previously.

config.go
@@ -195,16 +195,21 @@ func (cfg *Config) MarkDirty() {
 // were supplied on the CLI. The supplied name must correspond to a known option
 // in cfg, otherwise this method panics.
 func (cfg *Config) SetRuntimeOverride(name, value string) {
-	if _, ok := cfg.unifiedSources[name]; !ok {
+	var optionExists bool
+	if cfg.dirty {
+		optionExists = (cfg.FindOption(name) != nil)
+	} else {
+		_, optionExists = cfg.unifiedSources[name]
+	}
+	if !optionExists {
 		panic(fmt.Errorf("Assertion failed: option %s does not exist", name))
 	}
 
 	cfg.runtimeOverrides[name] = value
-
-	// Instead of marking the config as dirty and rebuilding it lazily, we can
-	// just set the value right away, since runtime overrides are always the
-	// highest priority source.
 	if !cfg.dirty {
+		// Instead of marking the config as dirty and rebuilding it lazily, we can
+		// just set the value right away, since runtime overrides are always the
+		// highest priority source.
 		cfg.unifiedValues[name] = value
 		cfg.unifiedSources[name] = cfg.runtimeOverrides
 	}

config_test.go
@@ -99,6 +99,16 @@ func TestRuntimeOverride(t *testing.T) {
 			t.Errorf("Expected config.OnCLI(%q) = %t, instead found %t", name, expected, actual)
 		}
 	}
+	assertSetRuntimeOverridePanic := func(c *Config, name string) {
+		t.Helper()
+		defer func() {
+			t.Helper()
+			if iface := recover(); iface == nil {
+				t.Errorf("Expected SetRuntimeOverride(%q) to panic, but it did not", name)
+			}
+		}()
+		c.SetRuntimeOverride(name, "foo")
+	}
 
 	cmd := simpleCommand()
 	cmd.AddOption(StringOption("optional1", 'y', "", "dummy description").ValueOptional())
@@ -115,6 +125,7 @@ func TestRuntimeOverride(t *testing.T) {
 	// Confirm behavior of overrides
 	cfg.SetRuntimeOverride("hasshort", "overridden1")
 	cfg.SetRuntimeOverride("optional2", "overridden2")
+	assertSetRuntimeOverridePanic(cfg, "doesnt-exist")
 	assertOptionValue(cfg, "hasshort", "overridden1")
 	assertOptionValue(cfg, "optional2", "overridden2")
 	assertOnCLI(cfg, "hasshort", false)
@@ -129,12 +140,17 @@ func TestRuntimeOverride(t *testing.T) {
 	assertOnCLI(clone, "hasshort", false)
 	assertOnCLI(clone, "optional2", false)
 	assertOnCLI(clone, "truthybool", true)
-
 	cfg.SetRuntimeOverride("optional2", "newval")
 	assertOptionValue(cfg, "optional2", "newval")
 	assertOptionValue(clone, "optional2", "overridden2")
 	clone.SetRuntimeOverride("hasshort", "alsonew")
 	assertOptionValue(cfg, "hasshort", "overridden1")
+	assertOptionValue(clone, "hasshort", "alsonew")
+
+	// Confirm behavior on a dirty config
+	clone = cfg.Clone()
+	assertSetRuntimeOverridePanic(clone, "doesnt-exist2")
+	clone.SetRuntimeOverride("hasshort", "alsonew")
 	assertOptionValue(clone, "hasshort", "alsonew")
 }
 